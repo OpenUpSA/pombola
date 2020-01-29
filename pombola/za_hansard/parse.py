e4b9399332b97139c4ac83d33718d974dc812a5b
Delena Malan <delena.malan@gmail.com>
2020-01-29 10:56:09 +0200

    Allow for whitespace after hansard date

--- pombola/za_hansard/parse.py
+++ pombola/za_hansard/parse.py
@@ -79,7 +79,7 @@ class DateParslet(SingleLineParslet):
         if parser.hasDate:
             return None
 
-        match = re.compile(r'(\d+)[ ,]+(\w+)[ ,]+(\d+)$').search(line)
+        match = re.compile(r'(\d+)[ ,]+(\w+)[ ,]+(\d+)\s*$').search(line)
         if not match:
             return None
 
