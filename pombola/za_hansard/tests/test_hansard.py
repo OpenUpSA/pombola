e6020e7b6aa6a8262d2bc4824eb644e50b679757
Delena Malan <delena.malan@gmail.com>
2020-01-29 13:57:56 +0200

    Add country attribute to hansard tests

--- pombola/za_hansard/tests/test_hansard.py
+++ pombola/za_hansard/tests/test_hansard.py
@@ -10,6 +10,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 
 from instances.models import Instance
+from nose.plugins.attrib import attr
 
 from pombola.za_hansard.models import Source
 
@@ -19,6 +20,7 @@ from lxml import etree
 import os
 
 
+@attr(country='south_africa')
 class ZAHansardParsingTests(TestCase):
 
     docnames = ['502914_1', 'NA290307', 'EPC110512', 'NA210212', 'NA200912']
@@ -163,6 +165,7 @@ class ZAHansardParsingTests(TestCase):
         self.assertEqual(len(subSections), 16)
 
 
+@attr(country='south_africa')
 class ZAHansardSayitLoadingTests(TestCase):
 
     tests_dir = os.path.dirname(os.path.abspath(__file__))
