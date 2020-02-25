a1af17fe74b2ab73e1c979e12178ba7cdac27719
Delena Malan <delena.malan@gmail.com>
2020-02-25 18:05:00 +0200

    Add DA Gauteng data

---
+++ pombola/south_africa/data/constituencies_and_offices/source_data/2019/da/gauteng/convert_to_json.py
@@ -0,0 +1,46 @@
+import csv
+import json
+from collections import defaultdict
+
+PROVINCE = "Gauteng"
+PROVINCE_SLUG = "gauteng"
+SOURCE_URL = "https://docs.google.com/spreadsheets/d/1hKuSYZkabY41YbrvRpP3sGpN3RwtTf99"
+PARTY = "DA"
+
+json_output = {"offices": [], "start_date": "2019-06-01", "end_date": "2019-05-31"}
+
+offices = defaultdict(list)
+
+with open("{}-raw.csv".format(PROVINCE_SLUG)) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row["Within constituency"].strip():
+            offices[row["Within constituency"].strip()].append(row)
+        elif row["Head of constituency"].strip():
+            offices[row["Head of constituency"].strip()].append(row)
+
+for title, office in offices.items():
+    json_office = {
+        "Title": "{} Constituency Area: {}".format(PARTY, title.strip()),
+        "Source URL": SOURCE_URL,
+        "Party": PARTY,
+        "Type": "area",
+        "Source Note": "{} Parliamentary Caucus 2019".format(PARTY),
+        "Province": PROVINCE,
+        "People": [],
+        "Location": title.strip(),
+    }
+    for person in office:
+        position = {
+            "Cell": person["Cellphone"].strip(),
+            "Email": person["Party email"].strip(),
+            "Name": "{} {}".format(person["First names"].strip(), person["Last name"].strip()),
+        }
+        if person["Head of constituency"].strip():
+            position["Position"] = "Constituency Head"
+        json_office["People"].append(position)
+    json_output["offices"].append(json_office)
+
+
+with open("{}.json".format(PROVINCE_SLUG), "w") as output:
+    json.dump(json_output, output, indent=4)
