2b30d77f5ce10bbce38ec0fe35704343446ae62b
Delena Malan <delena.malan@gmail.com>
2020-04-07 14:57:18 +0200

    Join all DA data

---
+++ pombola/south_africa/data/constituencies_and_offices/source_data/2019/da/free-state/convert_to_json.py
@@ -0,0 +1,41 @@
+import csv
+import json
+from collections import defaultdict
+
+json_output = {"offices": [], "start_date": "2019-06-01", "end_date": "2019-05-31"}
+
+offices = defaultdict(list)
+
+with open("north-west-raw.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row["Within constituency"].strip():
+            offices[row["Within constituency"].strip()].append(row)
+        elif row["Head of constituency"].strip():
+            offices[row["Head of constituency"].strip()].append(row)
+
+for title, office in offices.items():
+    json_office = {
+        "Title": "DA Constituency Area: %s" % title.strip(),
+        "Party": "DA",
+        "Type": "area",
+        "Source Note": "DA Parliamentary Caucus 2019",
+        "Province": "North West",
+        "People": [],
+        "Location": title.strip(),
+    }
+    for person in office:
+        position = {
+            "Cell": person["Cellphone"].strip(),
+            "Email": person["Public email"].strip(),
+            "Name": "%s %s"
+            % (person["First names"].strip(), person["Last name"].strip()),
+        }
+        if person["Head of constituency"].strip():
+            position["Position"] = "Constituency Chair"
+        json_office["People"].append(position)
+    json_output["offices"].append(json_office)
+
+
+with open("north-west.json", "w") as output:
+    json.dump(json_output, output, indent=4)
