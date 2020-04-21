da3be62c44af1553189abda34599213a36e8c824
Delena Malan <delena.malan@gmail.com>
2020-04-21 12:12:36 +0200

    Add 403 page test and fix 'basics' tests

--- pombola/core/seltests/basics.py
+++ pombola/core/seltests/basics.py
@@ -1,29 +1,45 @@
 from django.core.urlresolvers import reverse
 from pombola.testing.selenium import PombolaSeleniumTestCase
+from django.test.utils import override_settings
+from nose.plugins.attrib import attr
+from django_webtest import WebTest
+from mock import patch
 
-class CoreTestCase(PombolaSeleniumTestCase):
-    
 
+@attr(country="south_africa")
+class CoreTestCase(WebTest):
     def test_home(self):
-        self.open_url(reverse('home'))
-        self.assertRegexpMatches(self.get_title(), 'Home')
+        response = self.app.get(reverse("home"))
+        self.assertIn("Home", response)
 
     def test_static(self):
         """Test that the static files are being served"""
-
-        self.open_url('/static/static_test.txt')
-        self.assertTrue( 'static serving works!' in self.page_source )
-
+        response = self.app.get("/static/static_test.txt")
+        self.assertTrue("static serving works!" in response)
 
     def test_404(self):
         """Test that proper 404 page is being shown"""
-        self.open_url('/hash/bang/bosh')
-        self.assertTrue( 'Page Not Found - 404' in self.text)
+        response = self.app.get("/hash/bang/bosh", status=404)
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue("Page Not Found" in response)
 
+    @override_settings(GOOGLE_RECAPTCHA_SECRET_KEY="test-key")
+    @patch("pombola.search.recaptcha.recaptcha_client")
+    def test_403(self, mocked_recaptcha_client):
+        """Test that proper 404 page is being shown"""
+        mocked_recaptcha_client.verify.return_value = False
+
+        search_location_url = reverse("core_geocoder_search")
+        response = self.app.get(
+            "{0}?q={1}".format(search_location_url, "Cape Town"), status=403
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue("Request Forbidden" in response)
 
     def test_user_not_logged_in(self):
         """Test that by default user is not logged in"""
-        self.open_url('/')
+        response = self.app.get("/")
 
-        self.assertEqual( self.get_current_user_id(), None )
-        self.assertEqual( self.get_current_user(), None )
+        self.assertTrue(response.context["user"].is_anonymous())
+        # self.assertEqual( self.get_current_user_id(), None )
+        # self.assertEqual( self.get_current_user(), None )
