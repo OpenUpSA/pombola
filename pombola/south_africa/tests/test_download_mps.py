4a49551d7bd899becd0a714084e6f3d35eefc0ca
Delena Malan <delena.malan@gmail.com>
2020-05-08 12:09:21 +0200

    Test that MPL is downloaded

--- pombola/south_africa/tests/test_download_mps.py
+++ pombola/south_africa/tests/test_download_mps.py
@@ -1,11 +1,14 @@
-from django.test import TestCase
-import tempfile
-from nose.plugins.attrib import attr
-from django.core.urlresolvers import reverse
-from django_date_extensions.fields import ApproximateDate
-import xlrd
-from pombola.core.models import Person, Organisation, OrganisationKind, Position
 import datetime
+import tempfile
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django_date_extensions.fields import ApproximateDate
+from nose.plugins.attrib import attr
+
+import xlrd
+from pombola.core.models import (Organisation, OrganisationKind, Person,
+                                 Position)
 
 
 @attr(country="south_africa")
@@ -31,8 +34,19 @@ class DownloadMPsTest(TestCase):
             person=self.mp_a,
             organisation=self.na,
             start_date=ApproximateDate(past=True),
-            end_date=ApproximateDate(year=2200, month=1, day=1),
+            end_date=ApproximateDate(future=True),
         )
+        self.mpl_a = Person.objects.create(
+            legal_name="Jonathan Brink", slug="jonathan-brink"
+        )
+        Position.objects.create(
+            person=self.mpl_a,
+            organisation=self.ncop,
+            start_date=ApproximateDate(past=True),
+            end_date=ApproximateDate(future=True),
+        )
+        self.mps = [self.mp_a]
+        self.mpls = [self.mpl_a]
 
     def test_download_all_mps(self):
         response = self.client.get(reverse("sa-download-members-xlsx"))
@@ -43,6 +57,7 @@ class DownloadMPsTest(TestCase):
         book = xlrd.open_workbook(filename=f.name)
         self.assertEquals(1, book.nsheets)
         sheet = book.sheet_by_index(0)
-        names = sheet.col(0)
-        self.assertEqual(names[0].value, u"Name")
-        self.assertEqual(names[1].value, self.mp_a.name)
+        names = [cell.value for cell in sheet.col(0)]
+        self.assertEqual(names[0], u"Name")
+        for mp in self.mps:
+            self.assertIn(mp.name, names)
