3302794251205d9a1280ca7dba1c135d041d56e6
Delena Malan <delena.malan@gmail.com>
2020-08-25 19:26:48 +0200

    Remove ongoing() from committee list

--- pombola/south_africa/views/committees.py
+++ pombola/south_africa/views/committees.py
@@ -4,11 +4,8 @@ from pombola.core.models import Organisation, OrganisationKind
 
 
 class SACommitteesView(ListView):
-    # .filter(
-    #     contacts__kind__slug='email'
-    # )
     queryset = Organisation.objects.committees()\
-        .ongoing().select_related('kind').order_by('kind__id').all()
+        .select_related('kind').order_by('kind__id').all()
     context_object_name = 'committees'
     template_name = 'south_africa/committee_list.html'
 
