a5df393a13d5cc84814bd201ce857bf2536d7fa3
Delena Malan <delena.malan@gmail.com>
2020-08-31 00:30:18 +0200

    Fix test_templatetags

--- pombola/core/tests/test_templatetags.py
+++ pombola/core/tests/test_templatetags.py
@@ -19,14 +19,18 @@ class BreadcrumbTest(TestCase):
         tests = (
             # input, expected output
             ( '/',        '<li>Home</li>'),
-            ( '/organisation',     home_li + '<li>Organisations</li>'),
-            ( '/organisation/bar', home_li + '<li><a href="/organisation/all/" title="Breadcrumb link to Organisations">Organisations</a>  <span class="sep">&raquo;</span> </li><li>Bar</li>'),
+            # 'Organisation' was overwritten with 'People' in `BREADCRUMB_URL_NAME_MAPPINGS` in pombola/settings/south_africa_base.py
+            # TODO: explicitly set BREADCRUMB_URL_NAME_MAPPINGS for this test
+            ( '/organisation',     home_li + '<li>People</li>'),
+            ( '/organisation/bar', home_li + '<li><a href="/organisation/all/" title="Breadcrumb link to People">People</a>  <span class="sep">&raquo;</span> </li><li>Bar</li>'),
 
             # existing urls that aren't in the mapping should be linked to.
             ( '/blog/first-post', home_li + '<li><a href="/blog/" title="Breadcrumb link to Blog">Blog</a>  <span class="sep">&raquo;</span> </li><li>First Post</li>'),
 
             # urls that don't exist shouldn't be linked to.
-            ( '/foo/bar', home_li + '<li>Foo  <span class="sep">&raquo;</span> </li><li>Bar</li>'),
+            # the fallback URL in pombola/south_africa/fallback_urls.py now captures any
+            # single-word URL so /foo will still be linked to
+            ( '/foo/bar', home_li + '<li><a href="/foo/" title="Breadcrumb link to Foo">Foo</a>  <span class="sep">&raquo;</span> </li><li>Bar</li>'),
 
             # Test that coordinates are passed through correctly
             # (don't drop '-', put space after ',')
