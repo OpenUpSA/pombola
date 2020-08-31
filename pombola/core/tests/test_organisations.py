02fae1a585c25582f399baaea87e03e8c894a954
Delena Malan <delena.malan@gmail.com>
2020-08-31 10:18:14 +0200

    Add test for Organisation committees filter

--- pombola/core/tests/test_organisations.py
+++ pombola/core/tests/test_organisations.py
@@ -1,27 +1,27 @@
 from nose.plugins.attrib import attr
 from django.test import TestCase
 
-from pombola.core import models
+from pombola.core.models import Organisation, OrganisationKind, Identifier
 
 from django.contrib.contenttypes.models import ContentType
 
 
 @attr(country="south_africa")
-class OrganisationTest(TestCase):
+class OrganisationModelTest(TestCase):
     def setUp(self):
-        self.organisation_kind = models.OrganisationKind(name="Foo", slug="foo",)
+        self.organisation_kind = OrganisationKind(name="Foo", slug="foo",)
         self.organisation_kind.save()
 
-        self.organisation = models.Organisation(
+        self.organisation = Organisation(
             name="Test Org", slug="test-org", kind=self.organisation_kind,
         )
         self.organisation.save()
 
-        self.mysociety_id = models.Identifier.objects.create(
+        self.mysociety_id = Identifier.objects.create(
             identifier="/organisations/1",
             scheme="org.mysociety.za",
             object_id=self.organisation.id,
-            content_type=ContentType.objects.get_for_model(models.Organisation),
+            content_type=ContentType.objects.get_for_model(Organisation),
         )
 
     def testIdentifier(self):
@@ -32,3 +32,32 @@ class OrganisationTest(TestCase):
         self.mysociety_id.delete()
         self.organisation.delete()
         self.organisation_kind.delete()
+
+
+@attr(country="south_africa")
+class OrganisationQuerySetTest(TestCase):
+    def setUp(self):
+        self.foo_kind = OrganisationKind.objects.create(name="Foo", slug="foo",)
+        self.na_kind = OrganisationKind.objects.create(
+            name="National Assembly", slug="national-assembly-committees"
+        )
+        self.ncop_kind = OrganisationKind.objects.create(
+            name="NCOP", slug="ncop-committees"
+        )
+
+        self.test_organisation = Organisation.objects.create(
+            name="Test Org", slug="test-org", kind=self.foo_kind,
+        )
+
+        self.na_organisation = Organisation.objects.create(
+            name="Basic Education", slug="basic-education", kind=self.na_kind,
+        )
+        self.ncop_organisation = Organisation.objects.create(
+            name="NCOP Appropriations", slug="ncop-appropriations", kind=self.ncop_kind,
+        )
+
+    def testGetCommittees(self):
+        result = Organisation.objects.committees().all()
+        self.assertIn(self.na_organisation, result)
+        self.assertIn(self.ncop_organisation, result)
+        self.assertNotIn(self.test_organisation, result)
