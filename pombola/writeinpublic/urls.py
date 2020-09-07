4133953b37b4353882b1ca35d68e6b5b45b8d989
Delena Malan <delena.malan@gmail.com>
2020-09-07 12:29:10 +0200

    Don't cache write in public views

--- pombola/writeinpublic/urls.py
+++ pombola/writeinpublic/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
+from django.views.decorators.cache import cache_page
 
 from . import views
 
@@ -9,22 +11,22 @@ write_message_wizard = views.WriteInPublicNewMessage.as_view(url_name='writeinpu
 urlpatterns = (
     url(
         r'^pending/$',
-        TemplateView.as_view(template_name='writeinpublic/pending.html'),
+        cache_page(0)(TemplateView.as_view(template_name='writeinpublic/pending.html')),
         name='writeinpublic-pending',
     ),
     url(
         r'^message/(?P<message_id>\d+)/$',
-        views.WriteInPublicMessage.as_view(),
+        cache_page(0)(views.WriteInPublicMessage.as_view()),
         name='writeinpublic-message'
     ),
     url(
         r'^(?P<step>.+)/$',
-        write_message_wizard,
+        cache_page(0)(write_message_wizard),
         name='writeinpublic-new-message-step',
     ),
     url(
         r'^$',
-        write_message_wizard,
+        cache_page(0)(write_message_wizard),
         name='writeinpublic-new-message',
     ),
 )
