bb032bd3fa65c070ece47cf2c345af260e2c2c60
kaysiz <ksiziva@gmail.com>
2020-10-01 09:29:53 +0200

    included a test for the feedback form

--- pombola/feedback/tests.py
+++ pombola/feedback/tests.py
@@ -1,16 +1,22 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from http import HTTPStatus
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class FeedbackFormTest(TestCase):
+    def test_success_post(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that we submit the form successfully.
         """
-        self.assertEqual(1 + 1, 2)
+        response = self.client.post(
+            "/feedback/", 
+            data={
+                "comment": "change the page header to h1.",
+                "email": "test@test.com",
+                "url": "http://0.0.0.0:8000/person/qubudile-richard-dyantyi/",
+                "g-recaptcha-response": "03AGdBq24_yN5E-tNGTrWp0gKsnyAFi_98_tENffZTIPAjUPo6Be1SQF8OUFkRNnhoVkaJVbaoU30_gt1FVEumjIFNaU3hcrijVw8qu7P8gNOfPVpqsXWcGt5oV70adSyzDr9MsxNTrIgOgyhcSIdW2SZ0tEGxAST02WbVljE90SfGhpD8DZkWKzODmiNpGXWWB2B6dV-10eBDqGsBwYXnNwgDhvHx6I-N9p5jfT7ZxvWBd9RetLPw94tJGjkxTYbet-s0emVGqxKneSarmaTrr9qwrlIPMfP2oUoBqxuUqKbMmfzdKyermSgPbGMvks48DU9WIDXkzDqWdKw_cX3PColKVwepqY9LS7IdWTEB9rsDxiFUzeXE7xKRvcGBne5IezW4RzfHekftVVuN9o9a7KJdOQZYOBPSEAzDL81QxyZl3db9fjxsYullZ4hEZvJnY-N_WlgEG8uj"
+            }
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(
+            response, "<div class='success'>Thank you for your feedback!</div>", html=True
+        )
