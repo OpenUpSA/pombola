5421bfd2f8ce56ff9ccfc38521a23b3e17100238
kaysiz <ksiziva@gmail.com>
2020-10-01 10:27:30 +0200

    added failed test for recaptcha

--- pombola/feedback/tests.py
+++ pombola/feedback/tests.py
@@ -1,22 +1,48 @@
-from http import HTTPStatus
 from django.test import TestCase
+from mock import patch
+from django.test.utils import override_settings
 
 
 class FeedbackFormTest(TestCase):
-    def test_success_post(self):
+
+    @override_settings(GOOGLE_RECAPTCHA_SECRET_KEY='test-key')
+    @patch('pombola.core.recaptcha.recaptcha_client')
+    def test_success_post_with_recaptcha(self, mocked_recaptcha_client):
         """
         Tests that we submit the form successfully.
         """
+        mocked_recaptcha_client.verify.return_value = True
         response = self.client.post(
             "/feedback/", 
             data={
                 "comment": "change the page header to h1.",
                 "email": "test@test.com",
                 "url": "http://0.0.0.0:8000/person/qubudile-richard-dyantyi/",
-                "g-recaptcha-response": "03AGdBq24_yN5E-tNGTrWp0gKsnyAFi_98_tENffZTIPAjUPo6Be1SQF8OUFkRNnhoVkaJVbaoU30_gt1FVEumjIFNaU3hcrijVw8qu7P8gNOfPVpqsXWcGt5oV70adSyzDr9MsxNTrIgOgyhcSIdW2SZ0tEGxAST02WbVljE90SfGhpD8DZkWKzODmiNpGXWWB2B6dV-10eBDqGsBwYXnNwgDhvHx6I-N9p5jfT7ZxvWBd9RetLPw94tJGjkxTYbet-s0emVGqxKneSarmaTrr9qwrlIPMfP2oUoBqxuUqKbMmfzdKyermSgPbGMvks48DU9WIDXkzDqWdKw_cX3PColKVwepqY9LS7IdWTEB9rsDxiFUzeXE7xKRvcGBne5IezW4RzfHekftVVuN9o9a7KJdOQZYOBPSEAzDL81QxyZl3db9fjxsYullZ4hEZvJnY-N_WlgEG8uj"
+                "g-recaptcha-response": "test"
             }
         )
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, 200)
         self.assertContains(
             response, "<div class='success'>Thank you for your feedback!</div>", html=True
+        )
+
+    @override_settings(GOOGLE_RECAPTCHA_SECRET_KEY='test-key')
+    @patch('pombola.core.recaptcha.recaptcha_client')
+    def test_fail_post_with_recaptcha(self, mocked_recaptcha_client):
+        """
+        Tests that the submit feedback fails.
+        """
+        mocked_recaptcha_client.verify.return_value = False
+        response = self.client.post(
+            "/feedback/", 
+            data={
+                "comment": "change the page header to h1.",
+                "email": "test@test.com",
+                "url": "http://0.0.0.0:8000/person/qubudile-richard-dyantyi/",
+                "g-recaptcha-response": "test"
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response, "Sorry, something went wrong. Please try again or email us at <a href='mailto:contact@pa.org.za'>contact@pa.org.za</a>", html=True
         )
