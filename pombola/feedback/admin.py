419a453f5dfecde01b649143344bc32674845ad0
Delena Malan <delena.malan@gmail.com>
2020-10-07 21:36:46 +0200

    Add admin action to bulk mark feedback as spam

--- pombola/feedback/admin.py
+++ pombola/feedback/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib import messages
 from pombola.feedback.models import Feedback
+from django.utils.translation import ngettext
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
@@ -10,3 +12,14 @@ class FeedbackAdmin(admin.ModelAdmin):
     ordering = ('-created',)
     raw_id_fields = ('user',)
     search_fields = ('comment', 'user__username', 'url', 'email')
+    actions = ['mark_as_spammy']
+
+    def mark_as_spammy(self, request, queryset):
+        updated = queryset.update(status='spammy')
+        self.message_user(request, ngettext(
+            '%d feedback item was successfully marked as spam.',
+            '%d feedback items were successfully marked as spam.',
+            updated,
+        ) % updated, messages.SUCCESS)
+
+    mark_as_spammy.short_description = "Mark selected feedback as spam"
