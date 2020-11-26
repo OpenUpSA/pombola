1f37eb1450889430ec1650a204bd9620cdfe0b4f
Delena Malan <delena.malan@gmail.com>
2020-11-26 16:35:19 +0200

    Fix starting dates

--- pombola/core/models.py
+++ pombola/core/models.py
@@ -1700,8 +1700,8 @@ class Position(ModelBase, IdentifierMixin):
         none_repr = '0000-00-00'
 
         # value can be yyyy-mm-dd, future or None
-        start = str(self.start_date) if self.start_date else None
-        end   = str(self.end_date)   if self.end_date   else None
+        start = self.start_date if type(self.start_date) is str else repr(self.start_date) if self.start_date else None
+        end   = self.end_date if type(self.end_date) is str else repr(self.end_date)   if self.end_date   else None
 
         # set the value or default to something sane
         sorting_start_date =        start or none_repr
