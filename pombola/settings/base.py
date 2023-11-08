c6af99aba9064cf98460fb42e558a4ae53ca2102
paulmwatson <github.com@paulmwatson.com>
2023-11-08 12:04:14 +0200

    Revert to ENV for SENTRY_PROFILES_SAMPLE_RATE

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -723,5 +723,6 @@ if os.environ.get("SENTRY_DSN"):
         integrations=[DjangoIntegration()],
         environment=os.environ.get("ENVIRONMENT"),
         traces_sample_rate=os.environ.get("SENTRY_TRACES_SAMPLE_RATE", 1.0),
-        profiles_sample_rate=1.0
+        profiles_sample_rate=os.environ.get(
+            "SENTRY_PROFILES_SAMPLE_RATE", 1.0),
     )
